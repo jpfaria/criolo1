aa10564c93a0fd4672840f9f05e2743854c41ddb
jpfaria <jpfaria@gmail.com>
2013-07-04 10:52:29 -0300

    .

--- src/main/java/br/com/jpfaria/controller/IndexController.java
+++ src/main/java/br/com/jpfaria/controller/IndexController.java
@@ -4,14 +4,17 @@ import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.com.jpfaria.service.PostCurriculumService;
+
 @Controller
 public class IndexController {
 
     private Logger logger = Logger.getLogger(IndexController.class);
 
-	@RequestMapping(value = "/")
+	@RequestMapping(value = "/enviaDados")
 	public void index() {
-		
+		PostCurriculumService service = new PostCurriculumService();
+		service.post();
 	}
 
 }
