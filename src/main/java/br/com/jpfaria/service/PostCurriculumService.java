ec9baa1dd6307025042cbc6f1c8e250d4df8518e
jpfaria <jpfaria@gmail.com>
2013-07-04 10:46:19 -0300

    .

---
+++ src/main/java/br/com/jpfaria/service/PostCurriculumService.java
@@ -0,0 +1,27 @@
+package br.com.jpfaria.service;
+
+import java.io.File;
+
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+public class PostCurriculumService {
+
+	public void post() {
+	
+		String url = "http://www.marcuscavalcanti.net/trash/rest-api/index.php/resume?format=json";
+		String file = "/home/jpfaria/Documentos/sed.pdf";
+		RestTemplate template = new RestTemplate();
+		
+		MultiValueMap<String, Object> form = new LinkedMultiValueMap<String, Object>();
+		form.add("email", "jpfaria@gmail.com");
+		form.add("questionAnswer", "test");
+		form.add("file", new FileSystemResource(new File(file)));
+		
+		template.postForLocation(url, form);
+		
+	}
+	
+}
