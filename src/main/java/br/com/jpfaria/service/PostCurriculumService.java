97b1a3517393826be405efb0168340c962e75bb1
jpfaria <jpfaria@gmail.com>
2013-07-04 11:45:44 -0300

    .

--- src/main/java/br/com/jpfaria/service/PostCurriculumService.java
+++ src/main/java/br/com/jpfaria/service/PostCurriculumService.java
@@ -2,6 +2,7 @@ package br.com.jpfaria.service;
 
 import java.io.File;
 
+import org.apache.log4j.Logger;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
@@ -9,8 +10,12 @@ import org.springframework.web.client.RestTemplate;
 
 public class PostCurriculumService {
 
+	private Logger logger = Logger.getLogger(PostCurriculumService.class);
+	
 	public void post() {
 	
+		logger.info("Enviando mensagem");
+		
 		String url = "http://www.marcuscavalcanti.net/trash/rest-api/index.php/resume?format=json";
 		String file = "/home/jpfaria/Documentos/sed.pdf";
 		
